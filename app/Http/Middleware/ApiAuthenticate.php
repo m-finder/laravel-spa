7de7109cbaa8bc7f0fc5bfa8e5636888f8093728
M-finder <m@m-finder.com>
2019-12-21 21:08:03 +0800

    update

--- app/Http/Middleware/ApiAuthenticate.php
+++ app/Http/Middleware/ApiAuthenticate.php
@@ -12,7 +12,6 @@ class ApiAuthenticate
     {
         $url = str_replace($request->route()->getPrefix(), '','/'. $request->route()->uri);
         $url_replace = preg_replace("/\{.*\}/", '{*}', $url);
-//        dd(Auth::user());
         if (Auth::user()->checkAuth($url_replace)) {
             return $next($request);
         }
