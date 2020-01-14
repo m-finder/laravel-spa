4ea20469e10c4d1202d3951dc7d35c549c245670
wuyunfang <wuyunfang@minzhetech.com>
2020-01-14 17:39:26 +0800

    update

--- resources/js/components/admin/views/reset/Info.vue
+++ resources/js/components/admin/views/reset/Info.vue
@@ -10,7 +10,17 @@
                     </div>
 
                     <div class="card-body">
-                        <img class="avatar" :src="form.avatar || '/images/avatar.png'">
+                        <img class="avatar" :src="getAvatar()"/>
+                        <b-button @click="toggle" variant="outline-success">上传头像</b-button>
+                        <avatar-uploader url="" img-format="jpg" img-bgc="#fff"
+                                   v-model="show"
+                                   field="avatar"
+                                   @crop-success="cropSuccess"
+                                   @crop-upload-success="cropUploadSuccess"
+                                   @crop-upload-fail="cropUploadFail"
+                                   :no-rotate="false"
+                                   :headers="headers"/>
+
                     </div>
                 </div>
             </b-col>
@@ -20,6 +30,7 @@
 
 <script>
     import storage from "../../utils/storage";
+    import uploader from 'vue-image-crop-upload';
 
     const defaultForm = {
         avatar: null,
@@ -27,9 +38,39 @@
     };
     export default {
         name: "Info",
+        components: {
+            'avatar-uploader': uploader
+        },
         data() {
             return {
+                headers: {},
+                show: false,
                 form: Object.assign(storage.get('user-info') || storage.sessionGet('user-info'), defaultForm)
+            }
+        },
+        methods: {
+            toggle(){
+                let {show} = this;
+                this.show = !show;
+            },
+            getAvatar(){
+                return this.form.avatar || '/images/avatar.png';
+            },
+            cropSuccess(data, field, key) {
+                this.form.avatar = data;
+                console.log('-------- 剪裁成功 --------');
+            },
+            cropUploadSuccess(data, field, key) {
+                console.log('-------- 上传成功 --------');
+                console.log(data);
+                console.log('field: ' + field);
+                console.log('key: ' + key);
+            },
+            cropUploadFail(status, field, key) {
+                console.log('-------- 上传失败 --------');
+                console.log(status);
+                console.log('field: ' + field);
+                console.log('key: ' + key);
             }
         },
         created() {
